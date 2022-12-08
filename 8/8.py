dfdb786fb07a09384827a40347f8ab988a93ac65
tristanpank <tristanpank@gmail.com>
2022-12-08 15:36:41 -0500

    Complete day 8

--- 8/8.py
+++ 8/8.py
@@ -29,7 +29,66 @@ col_len = len(trees)
 #         if col[j] > max(col[j+1:]):
 #             visible.add((j, i))
 
+total_scores = []
+for i in range(col_len):
+    for j in range(row_len):
+        if i == 0 or j == 0 or i == col_len - 1 or j == row_len - 1:
+            continue
+        
+        distance_score = 1
+        distances = []
+        col = [row[j] for row in trees]
+        row = trees[i]
+        curr_tree = trees[i][j]
 
+        # check up
+        dist = 0
+        for k in range(i-1, -1, -1):
+            if col[k] >= curr_tree:
+                dist += 1
+                break
+            else:
+                dist += 1
+        distance_score *= dist
+        distances.append(dist)
 
-print(visible)
-print(len(visible))
+        # check down
+        dist = 0
+        for k in range(i+1, col_len):
+            if col[k] >= curr_tree:
+                dist += 1
+                break
+            else:
+                dist += 1
+        distance_score *= dist
+        distances.append(dist)
+
+        # check left
+        dist = 0
+        for k in range(j-1, -1, -1):
+            if row[k] >= curr_tree:
+                dist += 1
+                break
+            else:
+                dist += 1
+        distance_score *= dist
+        distances.append(dist)
+
+        # check right
+        dist = 0
+        for k in range(j+1, row_len):
+            if row[k] >= curr_tree:
+                dist += 1
+                break
+            else:
+                dist += 1
+        distance_score *= dist
+        distances.append(dist)
+
+        if distance_score == 374400:
+            print((i, j, distance_score, distances))
+        # print((i, j, distance_score, distances))
+        total_scores.append(distance_score)
+
+total_scores.sort()
+print(max(total_scores))
